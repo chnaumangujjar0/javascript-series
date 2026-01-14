76562a8376a00334024f8c719355f874c795f2e2
chnaumangujjar0 <chnaumangujjar0@gmail.com>
2026-01-14 10:06:17 +0500

    nodemon install

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 let a="hellooooo";
-let b="nuan";
-c="ddddddd0";
+let c=33;
+let b=String(c);
+;
+// console.log(typeof c);
+// console.log(typeof b);
+// console.log(b);
+// console.log("333" === 333)
+// console.log("hello" == "hello")
+// console.log("nnnn" == "33")
 
-console.log(a,b,c);
+// console.log("02" > 1);
+// console.log("2" > "2")
+// let name=new String("abcdef");
+// console.log(name.slice(0,4));
+// console.log(name.toUpperCase())
+// console.log(name.toLowerCase())
+// console.log(name.indexOf("g"))
+// console.log(name.trimStart(2))
+// console.log(name.at(-2));
+// console.log(name.charAt(4))
+// console.log(name.charCodeAt(4))
 
-console.table([a,b,c]);
+const str2 = "Is this a question?";
+
+console.log(str2.endsWith("?"));
+
