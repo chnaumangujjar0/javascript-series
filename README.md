826e552bb6efc3a5b98fa6f78b94f5aaaef686e1
chnaumangujjar0 <chnaumangujjar0@gmail.com>
2026-01-19 20:42:23 +0500

    dates completed

--- README.md
+++
@@ -1,2 +0,0 @@
-# javascript-series
-Muhammad Nauman
---
+++ dates.js
@@ -0,0 +1,15 @@
+// let myDate =new Date();
+// // console.log(myDate)
+// // console.log(myDate.toString())
+// // console.log(myDate.toDateString())
+// // console.log(myDate.toISOString());
+// // console.log(myDate.toLocaleDateString())
+// // console.log(myDate.toLocaleString())
+
+// // console.log(Date.now())
+// // console.log(myDate.getMinutes())  // it is start from 0 //
+// // console.log(myDate.getDay())
+// // console.log(myDate.getFullYear())
+// // console.log(myDate.getTime())
+
+// console.log(myDate.toLocaleString("default",{  dateStyle:"full"})) 
