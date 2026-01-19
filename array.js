b59fc9dd3ea7e8974912e141f1ed1c49a907746c
chnaumangujjar0 <chnaumangujjar0@gmail.com>
2026-01-19 22:30:26 +0500

    new methods

---
+++ array.js
@@ -0,0 +1,23 @@
+// const arr=[0,1,2,3,4,5,6]
+// arr.push(5);  for adding element
+// arr.push(6)  
+// arr.pop()  for removing element from last
+// arr.unshift(3)  for adding value at first
+// arr.shift()  for removing first element
+// arr.includes(3);
+// arr.indexOf(2)
+// arr.slice(1,3) it does not manipulate the original array
+// arr.splice(1,3) it changes the original array
+// arr.flat(length) it is use to collect the array in one form instead of branches like following example const arr=[1,2,[3,4,[5,6],7],8]
+// arr.from(name) for conversion to array
+// console.log(arr)
+// let arr2=arr.splice(1,3)
+// console.log(arr2)
+// console.log("A",arr)
+// const siblings=["numan","hamza","bilal"]
+// const czns=["umer","awais","javed"]
+// siblings.push(czns.concat())
+
+// console.log(siblings)
+// console.log(czns)
+// console.log(Array.from({name:"nauman"}))    interesting case
